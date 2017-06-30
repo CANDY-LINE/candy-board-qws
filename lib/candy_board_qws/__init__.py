a78afac1c9abbd5a3fbe9cc01ea334ca1fd0adbf
Daisuke Baba <baba.daisuke@gmail.com>
2017-06-30 16:22:57 +0900

    Re-order the baud rates

--- lib/candy_board_qws/__init__.py
+++ lib/candy_board_qws/__init__.py
@@ -22,13 +22,13 @@ import traceback
 # the same numbers).
 
 BPS_SYMS = {
-    9600:     termios.B9600,
-    19200:    termios.B19200,
-    38400:    termios.B38400,
-    57600:    termios.B57600,
     115200:   termios.B115200,
-    230400:   termios.B230400,
     460800:   4100,
+    230400:   termios.B230400,
+    57600:    termios.B57600,
+    38400:    termios.B38400,
+    19200:    termios.B19200,
+    9600:     termios.B9600,
     921600:   4103
     }
 
