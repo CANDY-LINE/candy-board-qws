0c820371eaef097312a53925e490fc9477c97066
Daisuke Baba <baba.daisuke@gmail.com>
2017-07-16 14:50:09 +0900

    Change License to ASL 2.0

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2017 CANDY LINE INC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 from setuptools import setup, find_packages
@@ -46,7 +60,7 @@ setup(
     long_description=readme_txt,
     packages=find_packages('lib'),
     package_dir={'': 'lib'},
-    license='MIT',
+    license='ASL 2.0',
     classifiers=[
                     'Programming Language :: Python',
                     'Development Status :: 5 - Production/Stable',
@@ -54,7 +68,7 @@ setup(
                     'Environment :: Console',
                     'Intended Audience :: System Administrators',
                     'Intended Audience :: Developers',
-                    'License :: OSI Approved :: MIT License',
+                    'License :: OSI Approved :: Apache Software License',
                     'Operating System :: POSIX :: Linux',
                     'Topic :: System :: Hardware',
                  ],
