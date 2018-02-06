c9ba6b374eb3f970070a0e934c0ceb94758ac722
Daisuke Baba <baba.daisuke@gmail.com>
2018-02-06 11:49:19 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = "1.2.3"
+version = "1.2.4"
 
 try:
     import pypandoc
